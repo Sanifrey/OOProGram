8455f773d340293c3528de6409105796d74b7f19
Sanifrey <Sanifrey>
2018-04-08 09:19:11 +0500

    Добавлены комментарии

--- GoodJ/src/MainP/Formula.java
+++ GoodJ/src/MainP/Formula.java
@@ -1,43 +1,102 @@
+/**
+* Устанавливаем принадлежность класса к пакету
+ */
 package MainP;
 
-import java.io.FileNotFoundException;
-
+/**
+* Подключаем библиотеку для работы с диалоговыми окнами
+ */
 import javax.swing.JOptionPane;
 
+/**
+ * Объявляем класс с модификатором public
+ */
 public class Formula {
-
+	/**
+	 * Экземпляр класса Planir
+	 */
 	private Planir plframe;
+	/**
+	 * Объявляем приватные статические переменные. FreeArea - значение свободной
+	 * площади в комнате. AllArea - значение всей площади комнаты.
+	 */
 	private static float FreeArea;
 	private static float AllArea;
 
+	/**
+	 * Вызываем конструктор
+	 */
 	public Formula(Planir someframe) {
 		this.plframe = someframe;
 	}
 
-	private void Formula(float area, float width, float length) {
+	/**
+	 * Приватный метод для расчёта свободного пространства в комнате
+	 */
+	private void FormulaFree(float area, float width, float length) {
+		/**
+		 * Присваиваем переменной AllArea значение переменной area
+		 */
 		AllArea = area;
+		/**
+		 * Выполняем расчёт свободной площади в комнате
+		 */
 		FreeArea = area - width * length;
+		/**
+		 * Если значение свободной площади(FreeArea) в комнате меньше нуля(что является
+		 * логической ошибкой), то присваиваем значение свободной площади(FreeArea) к
+		 * нулю.
+		 */
 		if (FreeArea < 0)
 			FreeArea = 0;
-		plframe.settextField_3(String.valueOf(FreeArea));
-		return;
-
+		/**
+		 * Вызываем метод FillTextField_3 для заполнения в форме планировщик поля
+		 * textField_3 значением FreeArea
+		 */
+		FillTextField_3(FreeArea);
 	}
 
+	/**
+	 * Публичный метод для вызова приватного метода FormulaFree.
+	 */
 	public void getFormula(String area, String width, String length) {
+		/**
+		 * Выполняем отслеживание блока кода, где может произойти ошибка, при помощи
+		 * исключения try
+		 */
 		try {
-			Formula(Float.parseFloat(area), Float.parseFloat(width), Float.parseFloat(length));
-		} catch (Exception ep) {
+			FormulaFree(Float.parseFloat(area), Float.parseFloat(width), Float.parseFloat(length));
+		}
+		/**
+		 * Исключение catch
+		 */
+		catch (Exception ep) {
+			/**
+			 * Обрабатываем исключение типа "Exception e" и показываем диалоговое окно
+			 */
 			JOptionPane.showMessageDialog(null, "\nПараметры объекта заполнены неверно!");
 		}
 	}
 
+	/**
+	 * Публичный метод(Геттер) для получения значения свободной площади в комнате.
+	 */
 	public static float getFreeArea() {
 		return FreeArea;
 	}
 
+	/**
+	 * Публичный метод(Геттер) для получения значения всей площади в комнате.
+	 */
 	public static float getAllArea() {
 		return AllArea;
+	}
+
+	/**
+	 * Метод для заполнения в форме планировщик поля textField_3 значением FreeArea
+	 */
+	private void FillTextField_3(float FreeArea) {
+		plframe.settextField_3(String.valueOf(FreeArea));
 	}
 
 }
